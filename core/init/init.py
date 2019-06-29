b90718e11744a5a47d87457792fe5b813704c831
Victor Zanella <victor@sysmo.com.br>
2019-06-29 11:14:51 -0300

    Implemented a "git ls-remote <url> <branch>" command to check if branch exists

--- core/init/init.py
+++ core/init/init.py
@@ -1,5 +1,17 @@
+import git
+
+# Private methods
+def isAvailableBranch(url, branch):
+    myGitObject = git.cmd.Git() 
+    #git ls-remote command with url and branch will return a hash if branch exists   
+    return myGitObject.ls_remote(url, branch).split('\n')[0]  
+
+# Exposed methods
 def empty():
     return 'init Class -> empty()'
 
 def full(origin = 'develop'):
-    return "".join(['init Class -> full(', origin, ')'])
+    if (isAvailableBranch('http://gitlab.sysmo.com.br/dsv-delphi/shared.git', origin)):
+        return "".join(['init Class -> full(', origin, ')', ' is available'])
+    else:
+        return "".join(['init Class -> full(', origin, ')', ' is not available'])

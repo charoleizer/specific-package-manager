16c191ebfda3f4c982be1b6cb77fc5c28be99ec6
Victor Zanella <victor.t.y.z@hotmail.com>
2019-06-30 02:19:24 -0300

    update

--- core/init/init.py
+++ core/init/init.py
@@ -1,17 +1,23 @@
 import git
 
 # Private methods
+
+
 def isAvailableBranch(url, branch):
-    myGitObject = git.cmd.Git() 
-    #git ls-remote command with url and branch will return a hash if branch exists   
-    return myGitObject.ls_remote(url, branch).split('\n')[0]  
+    myGitObject = git.cmd.Git()
+    # git ls-remote command with url and branch will return a hash if branch exists
+    return myGitObject.ls_remote(url, branch).split('\n')[0]
 
 # Exposed methods
+
+
 def empty():
     return 'init Class -> empty()'
 
-def full(origin = 'develop'):
-    if (isAvailableBranch('http://gitlab.sysmo.com.br/dsv-delphi/shared.git', origin)):
+
+def full(origin='develop'):
+    # For full command, we have to use a .ini file with full dependece URLs
+    if (isAvailableBranch('LOAD URL FROM FILE', origin)):
         return "".join(['init Class -> full(', origin, ')', ' is available'])
     else:
         return "".join(['init Class -> full(', origin, ')', ' is not available'])

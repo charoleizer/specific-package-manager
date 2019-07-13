97087dcc9c639d810f987694c63d7bab82790045
Victor Tadashi Yamashita Zanella <victor.t.y.z@hotmail.com>
2019-07-13 10:55:39 -0300

    test adaptation to the new response format

--- tests/init_test.py
+++ tests/init_test.py
@@ -3,15 +3,10 @@ import sys
 import json
 sys.path.append("..")  # Adds higher directory to python modules path.
 
-
 def test_full():
     assert init.full('test', 'master') == 'All Done'
-
-    assert json.loads(open('output/packages.json', "r").read()) == [
-        'https://github.com/charoleizer/specific-package-manager.git: master']
-
-    assert json.loads(open('output/failed-packages.json', "r").read()) == []
-
+    assert json.loads(open('output/packages.json', "r").read()) == {'dependencies': ['https://github.com/charoleizer/specific-package-manager.git: master']}
+    assert json.loads(open('output/failed-packages.json', "r").read()) == {'dependencies': []}
 
 def test_empty():
-    assert init.empty() == 'init Class -> empty()'
+    assert init.simple() == 'init Class -> simple()'
